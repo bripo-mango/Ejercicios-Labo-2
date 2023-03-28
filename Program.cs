d90b61d34c9bf35a325f2b7cdf5a71250f8edfd7
Braian Céspedes <jesusgreenn@gmail.com>
2023-03-28 19:45:55 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            bool auxNum;
+
+
+            Console.WriteLine("Ingrese un numero: ");
+            auxNum = int.TryParse(Console.ReadLine(), out numero);
+
+        }
+
+        static bool EsPrimo(int num)
+        {
+            if (num % 2 == 0)
+                return true;
+
+            return false;
+            
+        }
+
+        static string NumerosPrimos(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                if(EsPrimo(i))
+
+            }
+        }
+    }
+}
